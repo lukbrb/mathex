82b7d941e85cb87e7441d6632a35a01692f52807
Lucas Barbier-Goy <lucasbarbr@outlook.com>
2025-08-31 13:07:22 +0200

    correction typage des fonctions<-visits et correction visit subastract node

--- mathex/interpreter.py
+++ mathex/interpreter.py
@@ -3,41 +3,41 @@ from mathex.values import Number
 
 
 class Interpreter:
-    def visit(self, node: Node, context: dict) -> float:
+    def visit(self, node: Node, context: dict) -> Number:
         method_name = f'visit_{type(node).__name__}'
         method = getattr(self, method_name)
         return method(node, context)
     
-    def visit_NumberNode(self, node: Number, context: dict) -> float:
+    def visit_NumberNode(self, node: Number, context: dict) -> Number:
         return Number(node.value)
     
-    def visit_AddNode(self, node: AddNode, context: dict) -> float:
+    def visit_AddNode(self, node: AddNode, context: dict) -> Number:
         left = self.visit(node.left, context)
         right = self.visit(node.right, context)
         return Number(left.value + right.value)
 
-    def visit_MinusNode(self, node: MinusNode, context: dict) -> float:
+    def visit_SubstractNode(self, node: SubstractNode, context: dict) -> Number:
         left = self.visit(node.left, context)
         right = self.visit(node.right, context)
         return Number(left.value - right.value)
 
-    def visit_MultiplyNode(self, node: MultiplyNode, context: dict) -> float:
+    def visit_MultiplyNode(self, node: MultiplyNode, context: dict) -> Number:
         left = self.visit(node.left, context)
         right = self.visit(node.right, context)
         return Number(left.value * right.value)
     
-    def visit_DivideNode(self, node: DivideNode, context: dict) -> float:
+    def visit_DivideNode(self, node: DivideNode, context: dict) -> Number:
         left = self.visit(node.left, context)
         right = self.visit(node.right, context)
         if right.value == 0:
             raise Exception("Division by zero")
         return Number(left.value / right.value)
     
-    def visit_PlusNode(self, node: PlusNode, context: dict) -> float:
+    def visit_PlusNode(self, node: PlusNode, context: dict) -> Number:
         value = self.visit(node.node, context)
         return Number(+value.value)
     
-    def visit_MinusNode(self, node: MinusNode, context: dict) -> float:
+    def visit_MinusNode(self, node: MinusNode, context: dict) -> Number:
         value = self.visit(node.node, context)
         return Number(-value.value)
     
