0ed1f1e9f8e264b267b9913894f078340ef18788
Lucas Barbier-Goy <lucasbarbr@outlook.com>
2025-11-28 18:44:56 +0100

    [Update] Updates mathex version to 0.2 alpha

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,31 +2,45 @@ import unittest
 import numpy as np
 from mathex.lexer import Lexer
 from mathex.parser import Parser
-from mathex.interpreter import Evaluator
+from mathex.interpreter import Interpreter
 
 class TestParser(unittest.TestCase):
     def test_lexer(self):
-        lexer = Lexer(r"\rho + \sin(x)")
-        tokens = lexer.tokenize()
-        self.assertEqual(tokens, [('IDENTIFIER', 'rho'), ('+', '+'), ('IDENTIFIER', 'sin'), ('(', '('), ('IDENTIFIER', 'x'), (')', ')')])
+        lexer = Lexer(r"\sin(5) + 1")
+        tokens = lexer.generate_tokens()
+
+        t_tokens = [repr(token) for token in tokens]
+        expected_tokens = [
+            "FUNCTION:sin",
+            "LPAREN",
+            "NUMBER:5.0",
+            "RPAREN",
+            "PLUS",
+            "NUMBER:1.0"
+        ]
+
+        self.assertEqual(t_tokens, expected_tokens)
 
     def test_parser(self):
-        lexer = Lexer(r"\rho + sin(x)")
-        tokens = lexer.tokenize()
+        lexer = Lexer(r"(1) + \sin(-1)")
+        tokens = lexer.generate_tokens()
         parser = Parser(tokens)
         ast = parser.parse()
-        evaluator = Evaluator({"rho": 1, "x": 0})
-        result = evaluator.evaluate(ast)
-        self.assertAlmostEqual(result, 1 + np.sin(0))
+        # ctx = {"rho": 1, "x": 0}
+        ctx = {}
+        evaluator = Interpreter()
+        result = evaluator.visit(ast, ctx)
+        self.assertAlmostEqual(result, 1 + np.sin(-1))
 
-    def test_evaluator(self):
-        evaluator = Evaluator({"x": np.pi/2, "y": 0})
-        lexer = Lexer(r"sin(x) + cos(y)")
-        tokens = lexer.tokenize()
+    def test_interpreter(self):
+        ctx = {"x": np.pi/2, "y": 0}
+        interpreter = Interpreter()
+        lexer = Lexer(r"\sin(1) + \cos(3)")
+        tokens = lexer.generate_tokens()
         parser = Parser(tokens)
         ast = parser.parse()
-        result = evaluator.evaluate(ast)
-        self.assertAlmostEqual(result, 1 + 1)
+        result = interpreter.visit(ast, ctx)
+        self.assertAlmostEqual(result, np.sin(1) + np.cos(3))
 
 if __name__ == "__main__":
     unittest.main()
