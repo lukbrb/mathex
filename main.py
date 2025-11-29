c6f67e5870b7295dacb426208c917a4fd8357153
Lucas Barbier-Goy <lucasbarbr@outlook.com>
2025-11-29 21:41:29 +0100

    [MINOR] Clean main file from comments

--- main.py
+++ main.py
@@ -2,26 +2,6 @@ from mathex.lexer import Lexer
 from mathex.parser import Parser
 from mathex.interpreter import Interpreter
 
-# def evaluate_expression(expression: str, variables: dict) -> float:
-#     lexer = Lexer(expression)
-#     tokens = lexer.tokenize()
-#     parser = Parser(tokens)
-#     ast = parser.parse()
-#     evaluator = Evaluator(variables)
-#     return evaluator.evaluate(ast)
-
-# def run_example(n: int) -> None:
-#     """Run the nth exemple defined in the `exemples` folder."""
-#     path = f"exemples/ex{n:02d}.mathex"
-#     with open(path) as f:
-#         program = f.read()
-#         print(f"[EXAMPLE {n:02d}]")
-#         print(program, end=" = ")
-#         print(evaluate_expression(program, {}))
-
-# def run_all_examples(n_max : int) -> None:
-#     for n in range(n_max):
-#         run_example(n+1)
 
 if __name__ == "__main__":
     while True:
@@ -29,7 +9,6 @@ if __name__ == "__main__":
             text = input("mathex >> ")
             lexer = Lexer(text)
             tokens = lexer.generate_tokens()
-            # print(list(tokens))
             parser = Parser(tokens)
             tree = parser.parse()
             if not tree: # no input
